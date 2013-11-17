164593c8a85d6b087f7a9f301063fc59b05049fa
kuwalab <arata.shinichi@gmail.com>
2013-11-17 14:37:39 +0900

    JavaScriptのリファクタリング

--- src/main/webapp/WEB-INF/resources/js/book.js
+++ src/main/webapp/WEB-INF/resources/js/book.js
@@ -25,50 +25,53 @@ var getBookList = function() {
     for (i = 0; i < data.length; i++) {
       $tbody.append(JST['tr'](data[i]));
     }
-
-    // 行ごとの更新ボタンの処理
-    $('table').on('click', 'input[name="update"]', function() {
-      var $tr = $(this).parents('tr');
-      var sendData = {
-        bookId: $tr.find('input[name="bookId"]').val(),
-        bookName: $tr.find('input[name="bookName"]').val(),
-        price: $tr.find('input[name="price"]').val()
-      };
-      
-      var initBookId = $tr.find('input[name="initBookId"]').val();
-  
-      $.ajax({
-        method: 'put',
-        contentType: 'application/json;charset=utf-8',
-        data: JSON.stringify(sendData),
-        url: 'books/' + initBookId,
-        dataType: 'json'
-      }).done(function(data) {
-        getBookList();
-      }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
-      });
-    });
-
-    // 行ごとのdeleteボタンの処理
-    $('table').on('click', 'input[name="delete"]', function() {
-      var bookId = $(this).parents('tr').find('input[name="initBookId"]').val();
-      $.ajax({
-        method: 'delete',
-        url: 'books/' + bookId,
-        dataType: 'json'
-      }).done(function(data) {
-        getBookList();
-      }).fail(function(jqXHR, textStatus, errorThrown) {
-       console.log(textStatus);
-      });
-    });
   }).fail(function(jqXHR, textStatus, errorThrown) {
     console.log(textStatus);
   });
 };
 
+var setTrEvent = function() {
+  // 行ごとの更新ボタンの処理
+  $('table').on('click', 'input[name="update"]', function() {
+    var $tr = $(this).parents('tr');
+    var sendData = {
+      bookId: $tr.find('input[name="bookId"]').val(),
+      bookName: $tr.find('input[name="bookName"]').val(),
+      price: $tr.find('input[name="price"]').val()
+    };
+    
+    var initBookId = $tr.find('input[name="initBookId"]').val();
+
+    $.ajax({
+      method: 'put',
+      contentType: 'application/json;charset=utf-8',
+      data: JSON.stringify(sendData),
+      url: 'books/' + initBookId,
+      dataType: 'json'
+    }).done(function(data) {
+      getBookList();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    });
+  });
+
+  // 行ごとのdeleteボタンの処理
+  $('table').on('click', 'input[name="delete"]', function() {
+    var bookId = $(this).parents('tr').find('input[name="initBookId"]').val();
+    $.ajax({
+      method: 'delete',
+      url: 'books/' + bookId,
+      dataType: 'json'
+    }).done(function(data) {
+      getBookList();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+     console.log(textStatus);
+    });
+  });
+};
+
 getBookList();
+setTrEvent();
 
 $('#insert').on('click', function() {
   var sendData = {
@@ -84,9 +87,9 @@ $('#insert').on('click', function() {
     url: 'books',
     dataType: 'json'
   }).done(function(data) {
-      $('#insBookId').val('');
-      $('#insBookName').val('');
-      $('#insPrice').val('');
+    $('#insBookId').val('');
+    $('#insBookName').val('');
+    $('#insPrice').val('');
     getBookList();
   }).fail(function(jqXHR, textStatus, errorThrown) {
     console.log(textStatus);
